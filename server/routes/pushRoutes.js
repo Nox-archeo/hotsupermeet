b4683f46adac9447ae82406d020a524ced8256be
Nox-archeo <seb.chappss@gmail.com>
2026-01-26 10:14:08 +0100

    FIX: Correction import middleware auth dans pushRoutes

--- server/routes/pushRoutes.js
+++ server/routes/pushRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/auth');
+const { auth } = require('../middleware/auth');
 const PushNotificationService = require('../services/pushNotificationService');
 
 // S'abonner aux notifications push
